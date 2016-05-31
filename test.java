761aede4de951dabb8cf7cc7e365187fe5951e05
tate1010 <tate1010@users.noreply.github.com>
2016-05-31 12:46:09 -0400

    Update test.java

--- test.java
+++ test.java
@@ -7,6 +7,13 @@ public class test extends Applet
     
        Heart C1 = new Heart();
       C1.draw(g);
+      Diamond C2 = new Diamond();
+      Spade C3 = new Spade();
+      C2.setCentre(300,200);
+
+      C2.draw(g);
+      C3.setCentre(500,300);
+      C3.draw(g);
     }
 
 }
